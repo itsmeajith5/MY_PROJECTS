d5cf2068dc32c30bca67f77ccd6daa86e14f0f3a
itsmeajith5 <itsmeajithsasi5@gamil.com>
2025-06-16 21:00:28 +0530

    ECHO MESSAGE REMOVED

--- backup_script.sh
+++ backup_script.sh
@@ -8,8 +8,6 @@ mkdir -p "$BACKUP_DEST"
 # Create backup with properly formatted date
 tar -cvjf "$BACKUP_DEST/backup_$(date +"%Y-%m-%d_%H-%M-%S").tar.bz2" "$BACKUP_LOC"
 
-# Print confirmation message
-echo "Backup completed: $BACKUP_DEST/backup_$(date +"%Y-%m-%d_%H-%M-%S").tar.bz2"
 
 # Remove backups older than 7 days
 find "$BACKUP_DEST" -type f -name "*.tar.bz2" -mtime +7 -exec rm {} \;
