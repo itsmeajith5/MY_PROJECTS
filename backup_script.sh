7c5e45bd2c9ec2562a04f28d325b8d185129acb4
itsmeajith5 <itsmeajithsasi5@gamil.com>
2025-06-16 20:57:25 +0530

    CORRECTED

--- backup_script.sh
+++ backup_script.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
-BACKUP_LOC="/home/ajith/TEST"
-BACKUP_DEST="/home/data_$(date +%F).tar.bz2"
-tar -cvjf "$BACKUP_DEST" "$BACKUP_LOC"
-echo "Backup completed: $BACKUP_DEST"
+BACKUP_LOC="/home/ajith/Music"
+BACKUP_DEST="/backup"
 
-date
+# Ensure backup directory exists
+mkdir -p "$BACKUP_DEST"
+
+# Create backup with properly formatted date
+tar -cvjf "$BACKUP_DEST/backup_$(date +"%Y-%m-%d_%H-%M-%S").tar.bz2" "$BACKUP_LOC"
+
+# Print confirmation message
+echo "Backup completed: $BACKUP_DEST/backup_$(date +"%Y-%m-%d_%H-%M-%S").tar.bz2"
+
+# Remove backups older than 7 days
+find "$BACKUP_DEST" -type f -name "*.tar.bz2" -mtime +7 -exec rm {} \;
+
