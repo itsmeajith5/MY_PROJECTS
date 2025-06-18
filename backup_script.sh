bf92bc95571377630a8d3fc45e6061ee3fad33a2
itsmeajith5 <itsmeajithsasi5@gamil.com>
2025-06-18 08:07:44 +0530

    ADDED #CRONTAB LOG

--- backup_script.sh
+++ backup_script.sh
@@ -12,3 +12,4 @@ tar -cvzf "$BACKUP_DEST/backup_$(date +"%Y-%m-%d_%H-%M-%S").tar.gz" "$BACKUP_LOC
 # Remove backups older than 7 days
 find "$BACKUP_DEST" -type f -name "*.tar.gz" -mtime +7 -exec rm {} \;
 
+# ADD THIS LINE TO CRONTAB >> /tmp/script.log 2>&1 TO REDIRECT ERROR  OR OUTPUT LOG TO A FILE
