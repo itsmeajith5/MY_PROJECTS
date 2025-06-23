6a516d65d5482f9971e81d9ec1980e291a309ea2
itsmeajith5 <itsmeajithsasi5@gmail.com>
2025-06-23 09:03:44 +0530

    location of backup_script.sh

--- backup_script.sh
+++ backup_script.sh
@@ -1,7 +1,1 @@
-#!/bin/bash
-BACKUP_LOC="/home/ajith/TEST"
-BACKUP_DEST="/home/data_$(date +%F).tar.bz2"
-tar -cvjf "$BACKUP_DEST" "$BACKUP_LOC"
-echo "Backup completed: $BACKUP_DEST"
-
-date
+#code is in the main branch 
